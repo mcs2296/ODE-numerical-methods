a73b54ce77cf75b148b0380f7596a4276616fa9d
Marlen Salazar <mcs2296@columbia.edu>
2020-05-21 16:31:29 -0500

    First commit

---
+++ Euler_Method.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 22:09:32 2020
+
+@author: Marlen Salazar
+Overview: Program that will use euler's method to approximate the solution of a
+differential equation using a given step size and initial value. Will then write
+each step into a list and return the list. Inefficient but I'm t r y i n g. 
+"""
+
+import math
